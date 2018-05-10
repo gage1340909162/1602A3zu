605038cf15ad8d108903a8d84448aae6b1a32a10
shiyue <613473317@qq.com>
2018-05-10 19:23:03 +0800

    master

--- day3/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-gulp.task('testless', function() {
-    gulp.src('src/css/style.less', { base: 'src/css' })
-        .pipe(less())
-        .pipe(gulp.dest('dist'))
-})
-gulp.task('default', ['testless'])
